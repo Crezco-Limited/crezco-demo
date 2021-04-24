a2a68c5d345c4f36d13fc34c47ee640711599c33
jonheslop <jon.heslop@pusher.com>
2021-04-24 09:05:39 +0100

    Forgot to pass the href into component

--- components/button.js
+++ components/button.js
@@ -1,7 +1,10 @@
 export default function Button({ href, children }) {
   const Tag = href ? "a" : "button";
   return (
-    <Tag className="inline-block mt-16 p-4 bg-indigo-400 font-medium text-lg text-white rounded-lg hover:bg-indigo-600 focus:bg-indigo-600">
+    <Tag
+      href={href}
+      className="inline-block mt-16 p-4 bg-indigo-400 font-medium text-lg text-white rounded-lg hover:bg-indigo-600 focus:bg-indigo-600"
+    >
       {children}
     </Tag>
   );

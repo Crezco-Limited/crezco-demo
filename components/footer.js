ebb043400c5bed364c78e459485c6ee23ec7b097
jonheslop <jon.heslop@pusher.com>
2021-04-25 08:53:17 +0100

    Link text changes on focus/hover

--- components/footer.js
+++ components/footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="flex items-center justify-center w-full h-24 border-t divide-x bg-white mt-16">
       <a
-        className="px-4"
+        className="px-4 hover:text-indigo-500 focus:text-indigo-500"
         href="https://jonheslop.com"
         target="_blank"
         rel="noopener noreferrer"

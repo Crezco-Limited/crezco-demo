73a0412bcbeeb7899c03958d185e223e6326c4a4
jonheslop <jon.heslop@pusher.com>
2021-04-26 15:12:26 +0100

    Improve loading contrast

--- components/loading.js
+++ components/loading.js
@@ -1,16 +1,18 @@
 export default function Loading({ show = false }) {
   if (show) {
     return (
-      <div className="absolute inset-0 flex items-center justify-center bg-indigo-100 bg-opacity-70 z-10">
+      <div className="absolute inset-0 flex items-center justify-center bg-white bg-opacity-80 z-10">
         <span
-          className="rounded-full border-4 border-indigo-400 w-8 h-8 animate-spin mr-8"
+          className="rounded-full border-2 border-indigo-400 w-8 h-8 animate-spin mr-8"
           style={{
             borderLeftColor: "transparent",
           }}
         >
           {" "}
         </span>
-        <span className="text-indigo-400 font-medium text-5xl">Loading</span>
+        <span className="text-indigo-400 font-bold tracking-tighter text-5xl">
+          Loading
+        </span>
       </div>
     );
   } else {

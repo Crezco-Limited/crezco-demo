b431dc93abe9c55a261218ad45e4c7f23dfbaf1b
jonheslop <jon.heslop@pusher.com>
2021-04-26 13:34:35 +0100

    less transparent loading'

--- components/loading.js
+++ components/loading.js
@@ -1,7 +1,7 @@
 export default function Loading({ show = false }) {
   if (show) {
     return (
-      <div className="absolute inset-0 flex items-center justify-center bg-indigo-100 bg-opacity-40 z-10">
+      <div className="absolute inset-0 flex items-center justify-center bg-indigo-100 bg-opacity-70 z-10">
         <span
           className="rounded-full border-4 border-indigo-400 w-8 h-8 animate-spin mr-8"
           style={{

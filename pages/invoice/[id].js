f62d9d2e012d088602bb3b04fcc3073804f48fee
jonheslop <jon.heslop@pusher.com>
2021-04-26 09:32:43 +0100

    Get payment demand and display it with QR too

--- pages/invoice/[id].js
+++ pages/invoice/[id].js
@@ -1,29 +1,43 @@
-import { useRouter } from "next/router";
 import Head from "next/head";
+import { QRCode } from "react-qr-svg";
+
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import Layout from "@/components/layout";
+import getPayDemandAsProps from "@/lib/get-pay-demand";
 
-// export async function getServerSideProps({ query }) {
-//   return await getUserProps(query);
-// }
+export async function getServerSideProps({ query }) {
+  return await getPayDemandAsProps(query.id);
+}
 
-export default function Invoice({ user, error }) {
+export default function Invoice({ demand, error }) {
   return (
-    <div className="font-sans flex flex-col items-center justify-center min-h-screen py-2 bg-gradient-to-b from-indigo-50 to-gray-50">
-      <Head>
-        <title>Invoice details - Crezco demo</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex flex-col items-center justify-center flex-1 px-20">
-        <div className="border rounded-lg max-w-lg">
-          <Header />
-          <div className="p-8 bg-white rounded-b-lg">
-            <p className="text-lg font-medium">Your invoice</p>
-          </div>
-        </div>
-      </main>
-      <Footer />
-    </div>
+    <Layout title="Invoice details">
+      <div className="p-8 bg-white rounded-b-lg">
+        <p className="text-lg font-medium mb-4">
+          Your invoice is ready to share
+        </p>
+        <dl className="flex flex-wrap">
+          <dt className="mr-2">Reference:</dt>
+          <dd className="">{demand.reference}</dd>
+        </dl>
+        <dl className="flex flex-wrap">
+          <dt className="mr-2">Amount:</dt>
+          <dd className="">&pound;{demand.amount}</dd>
+        </dl>
+        <figure className="my-8">
+          <QRCode style={{ width: 256 }} value={demand.paymentUri} />
+        </figure>
+        <p className="text-xs">
+          Link:{" "}
+          <a
+            href={demand.paymentUri}
+            className="underline text-indigo-400 hover:text-indigo-500 focus:text-indigo-500"
+          >
+            {demand.paymentUri}
+          </a>
+        </p>
+      </div>
+    </Layout>
   );
 }

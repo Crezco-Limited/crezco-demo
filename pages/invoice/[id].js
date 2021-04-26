47644c73136397fc55d24dd60070e1f2836e0ce4
jonheslop <jon.heslop@pusher.com>
2021-04-26 08:59:13 +0100

    Base invoice page
    Need to fetch pay demand details

---
+++ pages/invoice/[id].js
@@ -0,0 +1,29 @@
+import { useRouter } from "next/router";
+import Head from "next/head";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+
+// export async function getServerSideProps({ query }) {
+//   return await getUserProps(query);
+// }
+
+export default function Invoice({ user, error }) {
+  return (
+    <div className="font-sans flex flex-col items-center justify-center min-h-screen py-2 bg-gradient-to-b from-indigo-50 to-gray-50">
+      <Head>
+        <title>Invoice details - Crezco demo</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex flex-col items-center justify-center flex-1 px-20">
+        <div className="border rounded-lg max-w-lg">
+          <Header />
+          <div className="p-8 bg-white rounded-b-lg">
+            <p className="text-lg font-medium">Your invoice</p>
+          </div>
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
+}

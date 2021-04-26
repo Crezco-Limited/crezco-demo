b03f782610f24bd1258e7e2c886031b2cfd6c413
jonheslop <jon.heslop@pusher.com>
2021-04-26 11:50:49 +0100

    Use hook to listen for PaymentCompleted and show message

--- pages/invoice/[id].js
+++ pages/invoice/[id].js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Head from "next/head";
 import { QRCode } from "react-qr-svg";
 
@@ -5,18 +6,46 @@ import Header from "@/components/header";
 import Footer from "@/components/footer";
 import Layout from "@/components/layout";
 import getPayDemandAsProps from "@/lib/get-pay-demand";
+import Pusher from "pusher-js";
 
 export async function getServerSideProps({ query }) {
   return await getPayDemandAsProps(query.id);
 }
 
 export default function Invoice({ demand, error }) {
+  const [paid, setPaid] = useState();
+  const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_API_KEY, {
+    cluster: "eu",
+  });
+
+  useEffect(() => {
+    // Subscribe to channel for this payDemandId
+    const channel = pusher.subscribe(`payments-${demand.payDemandId}`);
+    channel.bind("PaymentCompleted", (data) => {
+      setPaid(true);
+    });
+    return () => {
+      // Unsubscribe when component unmounts
+      pusher.unsubscribe(`payments-${demand.payDemandId}`);
+    };
+  }, []);
+
   return (
     <Layout title="Invoice details">
       <div className="p-8 bg-white rounded-b-lg">
         <p className="text-lg font-medium mb-4">
           Your invoice is ready to share
         </p>
+        {paid && (
+          <p className="inline-block font-medium text-lg mb-4 p-4 rounded bg-green-100 text-green-500 border border-green-500">
+            This invoice has been paid
+          </p>
+        )}
+        {!paid && (
+          <p className="inline-block font-medium text-lg mb-4 p-4 rounded bg-yellow-100 text-yellow-500 border border-yellow-500">
+            Payment pending
+          </p>
+        )}
         <dl className="flex flex-wrap">
           <dt className="mr-2">Reference:</dt>
           <dd className="">{demand.reference}</dd>

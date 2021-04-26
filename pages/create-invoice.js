2515d1e8c047013340effcd692909c678ae07f5a
jonheslop <jon.heslop@pusher.com>
2021-04-26 08:21:49 +0100

    set up form

--- pages/create-invoice.js
+++ pages/create-invoice.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Head from "next/head";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
@@ -10,6 +11,13 @@ export async function getServerSideProps({ query }) {
 }
 
 export default function CreateInvoice({ user, error }) {
+  const [amount, setAmount] = useState();
+  const [reference, setReference] = useState();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
     <div className="font-sans flex flex-col items-center justify-center min-h-screen py-2 bg-gradient-to-b from-indigo-50 to-gray-50">
       <Head>
@@ -20,7 +28,7 @@ export default function CreateInvoice({ user, error }) {
       <main className="flex flex-col items-center justify-center flex-1 px-20">
         <div className="border rounded-lg max-w-lg">
           <Header />
-          <form className="p-8 bg-white rounded-b-lg">
+          <form className="p-8 bg-white rounded-b-lg" onSubmit={handleSubmit}>
             <p className="text-lg font-medium">Hello {user.displayName}</p>
             {error && (
               <p className="text-lg text-red-500 mt-4 bg-red-50 p-1">
@@ -34,11 +42,15 @@ export default function CreateInvoice({ user, error }) {
               type="number"
               placeholder={100}
               prefix="&pound;"
+              onChange={(e) => setAmount(e.target.value)}
+              onBlur={(e) => setAmount(e.target.value)}
             />
             <Input
               label="Enter your reference"
               name="reference"
               placeholder="REF-123"
+              onChange={(e) => setReference(e.target.value)}
+              onBlur={(e) => setReference(e.target.value)}
             />
             <Button type="submit">Create payment link</Button>
           </form>
